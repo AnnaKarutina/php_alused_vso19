11b152f48891e5d2f0b157b540510e4b92b56c16
Anna Karutina <anna.karutina@khk.ee>
2020-01-31 10:51:25 +0200

    Tekstitöötlus - ülesannete lahednused

---
+++ tekst/ülesanded.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * file name: ylesanded.php;
+ * autor: anna.karutina;
+ * date: 03.12.2019;
+ */
+
+//Kasutaja lisab vormi nime, seda näiteks suured ja väikesed tähed läbisegi. Sina kood tervitab teda kenasti nimepidi, kus nimi algab suure algustähega.
+//Näiteks: sisend–>mARiO; väljund–>Tere, Mario!
+
+$nimi = $_GET['nimi'];
+$nimi = strtolower($nimi); // nimi on ainult väikestest tähtedest
+$nimi = ucfirst($nimi); // esimene täht on suur
+echo 'Tere, '.$nimi.'!'; // moodustame ja väljastame tervitus
+echo '<hr>';
+
+//Kuna on teada, et PHP käsitleb teksti kui massiivi, siis peaks saama seda tsükli abil nö. tükeldada. Ülesandeks on etteantud teksti iga tähe järgi lisada punkt.
+//Näiteks: sisend–>stalker; väljund–>S.T.A.L.K.E.R.
+
+$sisend = $_GET['sisend'];
+for($indeks = 0; $indeks < strlen($sisend); $indeks++){
+    $symbol = strtoupper($sisend[$indeks]); // teisendame täht suureks
+    echo $symbol.'.'; // väljastame koos punktiga
+}
+echo '<hr>';
+
+//Koosta tekstiväli, mis kuvab kasutaja sisestatud sõnumeid. Kasutaja ropud sõnad asendatakse tärnidega.
+//Näiteks: sisend–>Sa oled täielik noob; väljund–>Sa oled täielik ***
+
+$roppSonad = array('noob', 'kurat'); // roppsõnade massiiv
+$lause = $_GET['lause']; // kasutaja sisend
+$lause = strtolower($lause); // teisendame kõik väikesteks tähtedeks
+// iga roppsõna kohta, mis meie massiivis on
+foreach ($roppSonad as $roppSona){
+    // moodustame asendussõna vastava pikkusega
+    $asendus = '';
+    for($kord = 0; $kord < strlen($roppSona); $kord++){
+        $asendus .= '*';
+    }
+    // otsime, millisest indeksist hakkab pihta roppsõna meie lause sees
+    $roppSonaIndex = strpos($lause, $roppSona, 0);
+    // kui sõna on leitud
+    if($roppSonaIndex !== false ){
+        // teeme vastav asendus
+        $lause = substr_replace($lause, $asendus, $roppSonaIndex, strlen($roppSona));
+    }
+}
+// teeme tagasi lause algava suure tähega
+$lause = ucfirst($lause);
+// väljastame lause
+echo $lause;
+echo '<hr>';
+
+//Kasutajalt saadud eesnime ja perenime põhjal luuakse talle email lõpuga @hkhk.edu.ee. Kusjuures täpitähed asendatakse ä->a, ö->o, ü->y, õ->o ja kogu email on väikeste tähtedega
+//Näiteks: sisend–>Ülle ja Doos; väljund–>ylle.doos@hkhk.edu.ee
+$nimiJaPerenimi = $_GET['nimijaperenimi'];
+$asendus = array(
+    'ä' => 'a',
+    'ö' => 'o',
+    'ü' => 'u',
+    'õ' => 'o',
+    'Ä' => 'A',
+    'Ö' => 'O',
+    'Ü' => 'U',
+    'Õ' => 'O',
+);
+foreach ($asendus as $otsi=>$asenda){
+    $nimiJaPerenimi = str_replace($otsi, $asenda, $nimiJaPerenimi);
+}
+$tyhikuIndeks = strpos($nimiJaPerenimi, ' ', 0);
+$nimi = strtolower(substr($nimiJaPerenimi, 0, $tyhikuIndeks));
+$nimiJaPerenimi = substr($nimiJaPerenimi, $tyhikuIndeks+1);
+$tyhikuIndeks = strpos($nimiJaPerenimi, ' ');
+$perenimi = strtolower(substr($nimiJaPerenimi,$tyhikuIndeks+1));
+$email = $nimi.'.'.$perenimi.'@khk.ee';
+echo $email;
+
